b9bf4207c5a7b0f717f2e06d731e65f6ddefdcb8
小山　聖莉奈 <koyama.serina@ryobi.co.jp>
2020-11-18 16:48:52 +0900

    サンプル修正

--- src/views/News.vue
+++ src/views/News.vue
@@ -13,21 +13,31 @@
             <div class="message-header">
               <p>
                 <b-icon pack="fas" icon="exclamation-circle" size="fa-lg"></b-icon>
-                2020年11月12日
+                2020年11月12日 令和2年度 ○○健康診査開始のお知らせ
               </p>
-              <button class="delete" aria-label="delete"></button>
+              <!-- <button class="delete" aria-label="delete"></button> -->
             </div>
             <div class="message-body">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+              <div class="block">
+                実施期間： 2020年12月25日から12月30日まで
+              </div>
+              <div class="block">
+                対象者： 予約をされた方のみ
+              </div>
+              <div class="block">
+                受付時間： 午前  9時から12時まで
+              </div>
             </div>
           </article>
           <article class="message is-primary">
             <div class="message-header">
-              <p>2020年11月10日</p>
-              <button class="delete" aria-label="delete"></button>
+              <p>2020年11月10日  「▲▲健診」受診予約について</p>
+              <!-- <button class="delete" aria-label="delete"></button> -->
             </div>
             <div class="message-body">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+              <p>「▲▲健診」の予約期間は終了しました。<br>
+              次回の予約受付については決まり次第お知らせいたします。
+              </p>
             </div>
           </article>
         </div>
